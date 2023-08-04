e950a46f580275bdfa167b55bc6801646369600a
petrovav99 <uservsever@yandex.ru>
2023-08-04 17:36:41 +0400

    фигня какая то, не выводится картинка, и не ругается

--- crmudemy/settings.py
+++ crmudemy/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "crmudemy/static/",
+]
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
