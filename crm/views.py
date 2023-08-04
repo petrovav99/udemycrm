55d3fc48c207eb283a0ba142a628267576336dd2
petrovav99 <uservsever@yandex.ru>
2023-08-04 16:12:12 +0400

    косяк найден, при замене старого index на index_old, нажав кнопку рефакторинга - везде где был индекс он был заменен на old)

--- crm/views.py
+++ crm/views.py
@@ -4,7 +4,7 @@ from .models import Order
 # Create your views here.
 def first_page(request):
     object_list = Order.objects.all()
-    return render(request, './index_old.html', {'object_list' :
+    return render(request, './index.html', {'object_list' :
                                                object_list})
 
 def thanks_page(request):
