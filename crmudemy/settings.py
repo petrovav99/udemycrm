e3fa26d4408152a3e024762fb06a6ad7ec092b42
petrovav99 <uservsever@yandex.ru>
2023-08-08 13:37:10 +0400

    наконец то прогрузил, думаю было 2 проблемы - папка статик не находилась как в уроке, и скорее всего я накосячил в шаблон

--- crmudemy/settings.py
+++ crmudemy/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import pathlib
 from pathlib import Path
 import os
 
@@ -118,19 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
-
-STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(BASE_DIR), "crmudemy", "static"),
-)
+# dstatic > dstatic > settings.py
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "crmudemy/static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
