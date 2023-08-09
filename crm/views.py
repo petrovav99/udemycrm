9ba11156000a1b6e0e57c78af492fc6f54b49b98
petrovav99 <uservsever@yandex.ru>
2023-08-09 16:49:58 +0400

    добавлен функционал заявок и коммментариев к ним

--- crm/views.py
+++ crm/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Order
+from .forms import OrderForm
 from cms.models import CmsSlider
 from price.models import PriceCard, PriceTable
 
@@ -10,11 +11,13 @@ def first_page(request):
     pc_2 = PriceCard.objects.get(pk=2)
     pc_3 = PriceCard.objects.get(pk=3)
     price_table = PriceTable.objects.all()
+    form = OrderForm()
     dict_obj = {'slider_list': slider_list,
                 'pc_1': pc_1,
                 'pc_2': pc_2,
                 'pc_3': pc_3,
                 'price_table': price_table,
+                'form': form,
                 }
     return render(request, './index.html', dict_obj)
 
